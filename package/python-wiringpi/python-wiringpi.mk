9cc6cf34173397a1da83a18c89ec079bd10ee775
丹治秀樹 <tanji@daitogiken.local>
2024-03-13 08:35:39 +0000

    wiringpiに対応。

--- package/python-wiringpi/python-wiringpi.mk
+++ package/python-wiringpi/python-wiringpi.mk
@@ -4,11 +4,13 @@
 #
 ################################################################################
 
-PYTHON_WIRINGPI_VERSION = 2.60.0
-PYTHON_WIRINGPI_SOURCE = wiringpi-$(PYTHON_WIRINGPI_VERSION).tar.gz
-PYTHON_WIRINGPI_SITE = https://files.pythonhosted.org/packages/67/cf/35854ec6c7f7285454e2204a18e977d935aefc99e2b2a14093832faab510
+PYTHON_WIRINGPI_VERSION = 6d3c9e704e60bac837aecec2c8db95b0cd0f57d0
+PYTHON_WIRINGPI_SITE = https://github.com/WiringPi/WiringPi-Python.git
+PYTHON_WIRINGPI_SITE_METHOD = git
 PYTHON_WIRINGPI_SETUP_TYPE = setuptools
 PYTHON_WIRINGPI_LICENSE = LGPL
 PYTHON_WIRINGPI_LICENSE_FILES = LICENSE.txt
+PYTHON_WIRINGPI_DEPENDENCIES = python3 host-swig
+PYTHON_WIRINGPI_GIT_SUBMODULES = YES
 
 $(eval $(python-package))
