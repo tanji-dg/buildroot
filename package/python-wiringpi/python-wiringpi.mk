78fbb216112715b89d7841b2c3c87847627e364a
丹治秀樹 <tanji@daitogiken.local>
2024-03-14 00:47:38 +0000

    Python-WiringPiサポート。

--- package/python-wiringpi/python-wiringpi.mk
+++ package/python-wiringpi/python-wiringpi.mk
@@ -4,13 +4,19 @@
 #
 ################################################################################
 
-PYTHON_WIRINGPI_VERSION = 6d3c9e704e60bac837aecec2c8db95b0cd0f57d0
-PYTHON_WIRINGPI_SITE = https://github.com/WiringPi/WiringPi-Python.git
+#PYTHON_WIRINGPI_VERSION = master
+#PYTHON_WIRINGPI_SITE = https://github.com/WiringPi/WiringPi-Python.git
+#PYTHON_WIRINGPI_SITE_METHOD = git
+#PYTHON_WIRINGPI_GIT_SUBMODULES = YES
+#PYTHON_WIRINGPI_SITE = /home/htanji/work/WiringPi-Python
+#PYTHON_WIRINGPI_SITE_METHOD = local
+PYTHON_WIRINGPI_VERSION = d88eb93b778a72b14caf4175838223b8f2e59f77
+PYTHON_WIRINGPI_SITE = https://github.com/tanji-dg/WiringPi-Python.git
 PYTHON_WIRINGPI_SITE_METHOD = git
+PYTHON_WIRINGPI_GIT_SUBMODULES = YES
 PYTHON_WIRINGPI_SETUP_TYPE = setuptools
 PYTHON_WIRINGPI_LICENSE = LGPL
 PYTHON_WIRINGPI_LICENSE_FILES = LICENSE.txt
 PYTHON_WIRINGPI_DEPENDENCIES = python3 host-swig
-PYTHON_WIRINGPI_GIT_SUBMODULES = YES
 
 $(eval $(python-package))
